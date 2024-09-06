133ef17af3c763f0f6840469641b80ee184b6d54
ALittleBot <160833462+ALittleBot@users.noreply.github.com>
2024-09-06 22:35:49 +0800

    添加杂鱼特性 (●'◡'●)

--- src/main/java/top/alittlebot/thinking/entity/Zako.java
+++ src/main/java/top/alittlebot/thinking/entity/Zako.java
@@ -1,10 +1,14 @@
 package top.alittlebot.thinking.entity;
 
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.AbstractSchoolingFish;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
@@ -28,7 +32,7 @@ public class Zako extends AbstractSchoolingFish {
     @Override
     protected SoundEvent getDeathSound() {
         return SoundEvents.CAT_DEATH;
-    }
+    }  // 原文如此
 
     @Override
     protected SoundEvent getHurtSound(@NotNull DamageSource damageSource) {
@@ -39,4 +43,23 @@ public class Zako extends AbstractSchoolingFish {
     protected @NotNull SoundEvent getFlopSound() {
         return SoundEvents.COD_FLOP;
     }
+
+    @Override
+    public @NotNull InteractionResult mobInteract(@NotNull Player player, @NotNull InteractionHand hand) {
+        ItemStack itemInHand = player.getItemInHand(hand);
+        if (itemInHand.getItem() == Items.END_ROD) {
+            if (this.level().isClientSide) {  // 只有在客户端才生成粒子
+                // 抄繁殖粒子的代码怎么了
+                for (int i = 0; i < 7; ++i) {
+                    double d0 = this.random.nextGaussian() * 0.02;
+                    double d1 = this.random.nextGaussian() * 0.02;
+                    double d2 = this.random.nextGaussian() * 0.02;
+                    this.level().addParticle(ParticleTypes.HEART, this.getRandomX(1.0), this.getRandomY() + 0.5, this.getRandomZ(1.0), d0, d1, d2);
+                }
+            }
+            return InteractionResult.sidedSuccess(this.level().isClientSide);
+        }
+        return super.mobInteract(player, hand);
+    }
+
 }
